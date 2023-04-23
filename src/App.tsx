9eac5926fa14bd29625f873bbceec1dba587b0be
pinomaker-hoo <56928532+pinomaker-hoo@users.noreply.github.com>
2023-04-24 00:29:28 +0900

    feat : Set Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+// ** Router Imports
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 const App = () => {
-  return <h1>Hello world</h1>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Hello world</h1>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
