7c1879409e42bfc9c7398a9ab7dc75160fd8dcb9
pinomaker-hoo <56928532+pinomaker-hoo@users.noreply.github.com>
2023-04-24 00:24:01 +0900

    feat : Set Redux Tool Kit

---
+++ src/store/middleware/index.ts
@@ -0,0 +1,13 @@
+// ** Redux Imports
+import { isRejectedWithValue } from "@reduxjs/toolkit";
+
+export const rtkQueryErrorLogger =
+  (_api: any) => (next: any) => (action: any) => {
+    if (isRejectedWithValue(action)) {
+      console.log({ title: "Async error!", message: action.error.message });
+    }
+
+    return next(action);
+  };
+
+export const middleware = [rtkQueryErrorLogger];
